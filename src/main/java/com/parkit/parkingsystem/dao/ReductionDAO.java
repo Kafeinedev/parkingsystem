a237b7d6f0e21221eb99399bd07791077d81c52e
Kafeinedev <kafeinedev@protonmail.com>
2021-09-14 09:16:48 +0200

    Making ReductionDAO.isrecurring() pass the unit test

--- src/main/java/com/parkit/parkingsystem/dao/ReductionDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ReductionDAO.java
@@ -1,11 +1,34 @@
 package com.parkit.parkingsystem.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.parkit.parkingsystem.config.DataBaseConfig;
+import com.parkit.parkingsystem.constants.DBConstants;
 
 public class ReductionDAO {
 	public DataBaseConfig dataBaseConfig = new DataBaseConfig();
+	private static final Logger logger = LogManager.getLogger("ReductionDAO");
 
-	public boolean isRecurring(String string) {
+	public boolean isRecurring(String vehicleRegNumber) {
+		Connection con = null;
+		try {
+			con = dataBaseConfig.getConnection();
+			PreparedStatement ps = con.prepareStatement(DBConstants.IS_RECURRING);
+			ps.setString(1, vehicleRegNumber);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				return rs.getBoolean(1);
+			}
+		} catch (Exception ex) {
+			logger.error("Error getting recurring user", ex);
+		} finally {
+			dataBaseConfig.closeConnection(con);
+		}
 		return false;
 	}
 }
