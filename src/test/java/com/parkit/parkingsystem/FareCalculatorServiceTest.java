06d34c66739a378efde991805f4854d790cb0253
Kafeinedev <kafeinedev@protonmail.com>
2021-09-17 17:07:25 +0200

    simplified FareCalculatorServiceTest

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Instant;
 import java.util.Date;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,138 +33,100 @@ public class FareCalculatorServiceTest {
 		fareCalculatorService = new FareCalculatorService(mockReductionDAO);
 		ticket = new Ticket();
 		ticket.setVehicleRegNumber("ABCD");
+		ticket.setInTime(Date.from(Instant.EPOCH));
 	}
 
 	@Test
 	public void calculateFareCar() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000));
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
-
 		when(mockReductionDAO.isRecurrent(any(String.class))).thenReturn(false);
-
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		ticket.setOutTime(Date.from(Instant.ofEpochSecond(3600)));
 		ticket.setParkingSpot(parkingSpot);
+
 		fareCalculatorService.calculateFare(ticket);
 		assertEquals(ticket.getPrice(), Fare.CAR_RATE_PER_HOUR);
 	}
 
 	@Test
 	public void calculateFareBike() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000));
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE, false);
-
 		when(mockReductionDAO.isRecurrent(any(String.class))).thenReturn(false);
-
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		ticket.setOutTime(Date.from(Instant.ofEpochSecond(3600)));
 		ticket.setParkingSpot(parkingSpot);
+
 		fareCalculatorService.calculateFare(ticket);
 		assertEquals(ticket.getPrice(), Fare.BIKE_RATE_PER_HOUR);
 	}
 
 	@Test
 	public void calculateFareUnkownType() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000));
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, null, false);
-
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		ticket.setOutTime(Date.from(Instant.ofEpochSecond(3600)));
 		ticket.setParkingSpot(parkingSpot);
+
 		assertThrows(NullPointerException.class, () -> fareCalculatorService.calculateFare(ticket));
 	}
 
 	@Test
 	public void calculateFareBikeWithFutureInTime() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() + (60 * 60 * 1000));
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE, false);
-
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		ticket.setOutTime(ticket.getInTime());
+		ticket.setInTime(Date.from(Instant.ofEpochSecond(3600)));
 		ticket.setParkingSpot(parkingSpot);
+
 		assertThrows(IllegalArgumentException.class, () -> fareCalculatorService.calculateFare(ticket));
 	}
 
 	@Test
 	public void calculateFareBikeWithLessThanOneHourParkingTime() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (45 * 60 * 1000));// 45 minutes parking time should give 3/4th
-																		// parking fare
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE, false);
-
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		when(mockReductionDAO.isRecurrent(any(String.class))).thenReturn(false);
+		ticket.setOutTime(Date.from(Instant.ofEpochSecond(2700)));
 		ticket.setParkingSpot(parkingSpot);
+
 		fareCalculatorService.calculateFare(ticket);
 		assertEquals((0.75 * Fare.BIKE_RATE_PER_HOUR), ticket.getPrice());
 	}
 
 	@Test
 	public void calculateFareCarWithLessThanOneHourParkingTime() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (45 * 60 * 1000));// 45 minutes parking time should give 3/4th
-																		// parking fare
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
-
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		when(mockReductionDAO.isRecurrent(any(String.class))).thenReturn(false);
+		ticket.setOutTime(Date.from(Instant.ofEpochSecond(2700)));
 		ticket.setParkingSpot(parkingSpot);
+
 		fareCalculatorService.calculateFare(ticket);
 		assertEquals((0.75 * Fare.CAR_RATE_PER_HOUR), ticket.getPrice());
 	}
 
 	@Test
 	public void calculateFareCarWithMoreThanADayParkingTime() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (24 * 60 * 60 * 1000));// 24 hours parking time should give 24 *
-																			// parking fare per hour
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
-
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		when(mockReductionDAO.isRecurrent(any(String.class))).thenReturn(false);
+		ticket.setOutTime(Date.from(Instant.ofEpochSecond(3600 * 24)));
 		ticket.setParkingSpot(parkingSpot);
+
 		fareCalculatorService.calculateFare(ticket);
 		assertEquals((24 * Fare.CAR_RATE_PER_HOUR), ticket.getPrice());
 	}
 
 	@Test
 	public void calculateFareBikeOneHourRecurringUser() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (3600000));
-
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE, false);
-
 		when(mockReductionDAO.isRecurrent(any(String.class))).thenReturn(true);
-
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		ticket.setOutTime(Date.from(Instant.ofEpochSecond(3600)));
 		ticket.setParkingSpot(parkingSpot);
+
 		fareCalculatorService.calculateFare(ticket);
-		assertEquals((Fare.BIKE_RATE_PER_HOUR * 1.0 - ReductionFactor.RECURRENT_USER), ticket.getPrice());
+		assertEquals((Fare.BIKE_RATE_PER_HOUR * (1.0 - ReductionFactor.RECURRENT_USER)), ticket.getPrice());
 	}
 
 	@Test
 	public void calculateFareCarLessThanThirtyMin() {
-		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (1799000));
-
-		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+		when(mockReductionDAO.isRecurrent(any(String.class))).thenReturn(false);
 
-		ticket.setInTime(inTime);
-		ticket.setOutTime(outTime);
+		ticket.setOutTime(Date.from(Instant.ofEpochSecond(1799)));
 		ticket.setParkingSpot(parkingSpot);
 		fareCalculatorService.calculateFare(ticket);
 		assertEquals(0.0, ticket.getPrice());
